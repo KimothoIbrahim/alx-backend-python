07ea45b6fc22e8a421442de694537ef4b010805e
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-08-19 12:59:22 +0300

    Using float type in python

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Typing using floats
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    args: n: float
+    Returns: float
+    """
+    return math.floor(n)
