14f5fbf3bcadc654ac8686c39865ae909b6a4329
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-08-19 16:00:26 +0300

    use mixed array

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,10 +3,10 @@
 Write a type-annotated function sum_mixed_list which takes a list
 mxd_lst of integers and floats and returns their sum as a float.
 """
-from typing import List
+from typing import Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
     """
     args: List[int, float]
     Returns: float
