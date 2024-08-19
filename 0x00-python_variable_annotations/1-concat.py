995c2f59ffc06cd2642de4c2e5dbde8afec7615d
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-08-19 12:51:03 +0300

    string type in python using typing module

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+TYPE string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    args: str1: str, str2: str
+    Returns: str
+    """
+    return str1+str2
