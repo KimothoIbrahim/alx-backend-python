48831e7f2f09df8da80dfbd89e3d90a02f7c68da
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-08-19 17:39:12 +0300

    make a tuple

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,10 +2,10 @@
 """
 Augment the following code with the correct duck-typed annotations
 """
-from typing import Union, Sequence, Any, List
+from typing import Optional, Sequence, Any
 
 
-def safe_first_element(lst: Sequence[Any]) -> List[Union[Any, None]]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     if lst:
         return lst[0]
     else:
