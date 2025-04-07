c0f219a0872d6d4964a76ac41c7f747b77316027
KimothoIbrahim <kimothoibrahim@yahoo.com>
2025-04-07 23:25:19 +0300

    Set parameterization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,3 +20,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """test the access_nested_map
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """test for bad Path and Map Failure
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
